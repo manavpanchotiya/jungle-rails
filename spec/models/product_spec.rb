943dc10f0762ea8725b8ee88b9320e9a7b983d0d
manavpanchotiya <manavpanchotiya@gmail.com>
2025-06-03 16:36:51 -0500

    changed to instance variable

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,75 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  before(:all) do
+    @category = Category.create(name: 'Test category')
+  end
+
   describe 'Validation' do
     it "is valid with all required fields" do
-      category = Category.create(name: 'Test category')
 
-      product = Product.new(
+      @product = Product.new(
         name: 'Test product',
         price: 100,
         quantity: 5,
-        category: category
+        category: @category
       )
 
-      expect(product).to be_valid
+      expect(@product).to be_valid
     end
 
     it "is not valid without name field" do
-      category = Category.create(name: 'Test category')
-
-      product = Product.new(
+      @product = Product.new(
         name: nil,
         price: 100,
         quantity: 5,
-        category: category
+        category: @category
       )
 
-      product.valid?
-
-      expect(product.errors.full_messages).to include("Name can't be blank")
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is not valid without price field" do
-      category = Category.create(name: 'Test category')
-
-      product = Product.new(
+      @product = Product.new(
         name: 'Test product',
         price: '',
         quantity: 5,
-        category: category
+        category: @category
       )
 
-      product.valid?
-      expect(product.errors.full_messages).to include("Price is not a number")      
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price is not a number")      
     end
 
     it "is not valid without quantity field" do
-      category = Category.create(name: 'Test category')
-
-      product = Product.new(
+      @product = Product.new(
         name: 'Test product',
         price: 100,
         quantity: nil,
-        category: category
+        category: @category
       )
 
-      product.valid?
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "is not valid without category field" do
-      category = Category.create(name: 'Test category')
-
-      product = Product.new(
+      @product = Product.new(
         name: 'Test product',
         price: 100,
         quantity: 5,
         category: nil
       )
 
-      product.valid?
-      expect(product.errors.full_messages).to include("Category can't be blank")
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
